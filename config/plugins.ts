bce4ed3651ebe73662dbf6d1cb4e7887bc952172
Aleš <foxxcz@gmail.com>
2024-03-09 13:32:18 +0100

    s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,15 +3,19 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('S3_ACCESS_KEY_ID'),
-        secretAccessKey: env('S3_ACCESS_SECRET'),
-        endpoint: env('S3_ENDPOINT_URL'),
-        region: 'us-east-1',
-        s3ForcePathStyle: true,
-        params: {
-          Bucket: env('S3_BUCKET_NAME')
-        }
-      }
-    }
+        s3Options: {
+          credentials: {
+            accessKeyId: env('S3_ACCESS_KEY_ID'),
+            secretAccessKey: env('S3_ACCESS_SECRET'),
+          },
+          endpoint: env('S3_ENDPOINT_URL'),
+          region: 'us-east-1',
+          s3ForcePathStyle: true,
+          params: {
+            Bucket: env('S3_BUCKET_NAME')
+          },
+        },
+      },
+    },
   },
 });
