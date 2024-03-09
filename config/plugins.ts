329990cdab110039e2e15f4e1e477dfb79c04ae4
Aleš <foxxcz@gmail.com>
2024-03-09 21:34:21 +0100

    s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,27 +1,21 @@
-export default ({ env }) => {
-  const r = {
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          s3Options: {
-            credentials: {
-              accessKeyId: env('S3_ACCESS_KEY_ID'),
-              secretAccessKey: env('S3_ACCESS_SECRET'),
-            },
-            endpoint: env('S3_ENDPOINT_URL'),
-            region: 'us-east-1',
-            s3ForcePathStyle: true,
-            params: {
-              Bucket: env('S3_BUCKET_NAME')
-            },
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('S3_ACCESS_KEY_ID'),
+            secretAccessKey: env('S3_ACCESS_SECRET'),
+          },
+          endpoint: env('S3_ENDPOINT_URL'),
+          region: 'us-east-1',
+          s3ForcePathStyle: true,
+          params: {
+            Bucket: env('S3_BUCKET_NAME')
           },
         },
       },
     },
-  };
-
-  console.log(r);
-  console.log(JSON.stringify(r));
-  return r;
-};
+  },
+});
