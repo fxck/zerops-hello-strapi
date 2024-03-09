5d55ae07b31732f596d163e9a4fd1bdd529536af
Aleš <foxxcz@gmail.com>
2024-03-09 10:17:23 +0100

    s3 middleware/plugin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,28 @@
 export default [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'storage-prg1.zerops.dev'],
+          'media-src': ["'self'", 'data:', 'blob:', 'storage-prg1.zerops.dev'],
+          upgradeInsecureRequests: null
+        }
+      }
+    }
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: '*'
+    }
+  },
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('S3_ACCESS_KEY_ID'),
+        secretAccessKey: env('S3_ACCESS_SECRET'),
+        endpoint: env('S3_ENDPOINT_URL'),
+        region: 'us-east-1',
+        s3ForcePathStyle: true,
+        params: {
+          Bucket: env('S3_BUCKET_NAME')
+        }
+      }
+    }
+  },
+});
